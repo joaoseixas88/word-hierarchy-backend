66f97a87a694bdd29d68b10cea0f5f64f61489b0
João Seixas <joaoseixas88@gmail.com>
2024-08-22 23:30:39 -0300

    test: ensure to parse data correctly if file exist and is in correct format

--- tests/word-hierarchy-maker-by-file.spec.ts
+++ tests/word-hierarchy-maker-by-file.spec.ts
@@ -1,6 +1,36 @@
 import { WordHierarchyMakerByFile } from "../src/services/word-hierarchy-maker-by-file";
 import { WordThreeValidator } from "../src/validator";
 
+const expectedValue = {
+  Animais: {
+    Aves: {
+      Pássaros: ["Canários", "Papagaios", "Pardais", "Rouxinóis"],
+      Rapinas: ["Águias", "Falcões", "Corujas", "Milhafres"],
+    },
+    Mamíferos: {
+      Carnívoros: { Felinos: ["Leões", "Tigres", "Jaguares", "Leopardos"] },
+      Herbívoros: {
+        Bovídeos: ["Bois", "Búfalos", "Antílopes", "Cabras"],
+        Equídeos: ["Cavalos", "Zebras", "Asnos"],
+      },
+      Primatas: ["Gorilas", "Chimpanzés", "Orangotangos"],
+    },
+  },
+  Construções: {
+    Edifícios: {
+      Comerciais: ["Escritórios", "Lojas", "Shopping Centers"],
+      Industriais: ["Fábricas", "Armazéns", "Galpões"],
+      Residenciais: ["Casas", "Apartamentos", "Sobrados"],
+    },
+    Infraestrutura: {
+      Energia: ["Usinas", "Subestações", "Linhas de Transmissão"],
+      Hidráulica: ["Barragens", "Canalizações", "Reservatórios"],
+      Transportes: ["Rodovias", "Ferrovias", "Aeroportos"],
+    },
+    Monumentos: ["Estátuas", "Praças", "Memoriais"],
+  },
+};
+
 const makeSut = () => {
   const filepath = process.cwd() + "/dicts/example.json";
   class Validator extends WordThreeValidator {
@@ -8,7 +38,6 @@ const makeSut = () => {
       return true;
     }
   }
-
   const validator = new Validator();
   const sut = new WordHierarchyMakerByFile(validator, filepath);
 
@@ -39,5 +68,10 @@ describe("WordHierarchyMakerByFile", () => {
         "Object readed is not in valid format, the object example and type is in README.md"
       )
     );
+  });
+  it("it parse data correctly if file exists and is in correct format", async () => {
+    const { sut } = makeSut();
+    const result = await sut.make();
+    expect(result).toEqual(expectedValue);
   });
 });
