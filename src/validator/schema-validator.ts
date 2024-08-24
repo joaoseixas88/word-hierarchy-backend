0278f9665d9a6bd6be12b181a50ac4c2cfba40eb
João Seixas <joaoseixas88@gmail.com>
2024-08-24 10:00:14 -0300

    feat: create an schema validator to validate zod schemas

---
+++ src/validator/schema-validator.ts
@@ -0,0 +1,16 @@
+import { Schema } from "zod";
+import { ValidationException } from "../presentation/errors/validation";
+
+export class SchemaValidator {
+  static validateSchema<T = any>(
+    schema: Schema<T>,
+    params: any,
+    statusCode?: number
+  ) {
+    try {
+      return schema.parse(params);
+    } catch (error) {
+      throw new ValidationException(error.issues, statusCode);
+    }
+  }
+}
