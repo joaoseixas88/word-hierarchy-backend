6f302129405656a09fed5cb79bce48e02a5cb792
João Seixas <joaoseixas88@gmail.com>
2024-08-24 10:00:42 -0300

    feat: create http types

---
+++ src/types/http.ts
@@ -0,0 +1,18 @@
+export type HttpResponse<T = any> = {
+  statusCode: number;
+  data?: T;
+};
+
+export type HttpContextContract = {
+  request: {
+    allParams(): any;
+    headers(): Record<string, string>;
+  };
+  response: {
+    ok(data: any): HttpResponse;
+    badRequest(data: any): HttpResponse;
+    noContent(data: any): HttpResponse;
+    internalServerError(): HttpResponse;
+    notFound(data?: any): HttpResponse;
+  };
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
+export * from "./http";
 export * from "./word-hierarchy-maker";
 export * from "./word-hierarchy-three";
