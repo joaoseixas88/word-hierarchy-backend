581ffe1c2401b0e697c23fb6e13f543d4a15d352
João Seixas <joaoseixas88@gmail.com>
2024-08-24 10:11:09 -0300

    refactor: change filename

---
+++ src/services/word-hierarchy-file-reader.ts
@@ -0,0 +1,42 @@
+import { readFile } from "fs/promises";
+import { injectable } from "tsyringe";
+import { WordHierarchyThree } from "../types";
+import { WordThreeValidator } from "../validator";
+
+@injectable()
+export class WordHierarchyReaderByFile {
+  constructor(private readonly validator: WordThreeValidator) {}
+  private _timeLapsed = 0;
+
+  async read(filepath: string): Promise<string | undefined> {
+    const start = new Date();
+    try {
+      const file = await readFile(filepath, { encoding: "utf-8" });
+      return file;
+    } catch (error) {
+      return undefined;
+    } finally {
+      const end = new Date();
+      this._timeLapsed = end.getMilliseconds() - start.getMilliseconds();
+    }
+  }
+  get timeLapsed() {
+    return this._timeLapsed;
+  }
+
+  async getFileData(filepath: string): Promise<WordHierarchyThree | undefined> {
+    const readedFile = await this.read(filepath);
+    if (!readedFile) {
+      return undefined;
+    }
+    const parsedData = JSON.parse(readedFile) as WordHierarchyThree;
+    const isValid = this.validator.isValid(parsedData);
+    if (!isValid) {
+      throw new Error(
+        "Object readed is not in valid format, the object example and type is in README.md"
+      );
+    }
+
+    return parsedData;
+  }
+}
