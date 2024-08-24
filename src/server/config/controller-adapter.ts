47c9c484735ba92716ba1411ad3fcef19e160898
João Seixas <joaoseixas88@gmail.com>
2024-08-24 10:02:48 -0300

    feat: create a helper method to adapt express routes to controllers

---
+++ src/server/config/controller-adapter.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { ValidationException } from "../../presentation/errors/validation";
+import { HttpContextContract, HttpResponse } from "../../types/http";
+
+export const httpCtx = (req: Request, res: Response): HttpContextContract => ({
+  request: {
+    allParams: () => ({
+      ...(req.params || {}),
+      ...(req.query || {}),
+      ...(req.body || {}),
+    }),
+    headers: () => req.headers as Record<string, string>,
+  },
+  response: {
+    ok: (data: any) => ({ data, statusCode: 200 }),
+    badRequest: (error: any) => ({ data: error, statusCode: 400 }),
+    noContent: () => ({ statusCode: 200 }),
+    notFound: (data?: any) => ({
+      statusCode: 404,
+      data: data ? data : "Not found",
+    }),
+    internalServerError: () => ({
+      statusCode: 500,
+      data: "Internal server error",
+    }),
+  },
+});
+
+export const adaptMethod =
+  <T>(controller: new (...args: any[]) => T, method: keyof T) =>
+  async (req: Request, res: Response) => {
+    try {
+      const instance = container.resolve(controller);
+      const ctx = httpCtx(req, res);
+      if (typeof instance[method] === "function") {
+        const httpResponse: HttpResponse = await instance[method](ctx);
+        if (!httpResponse) {
+          return res.status(200).send();
+        }
+        return res.status(httpResponse.statusCode).json(httpResponse.data);
+      }
+    } catch (error) {
+      console.log("error:", error);
+      if (error instanceof ValidationException) {
+        return res.status(error.statusCode).json(error.issues);
+      }
+      return res.status(500).send("Internal server error");
+    }
+  };
