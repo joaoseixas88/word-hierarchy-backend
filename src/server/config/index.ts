54cf5c185bb33c195b68cff580502a9729bff264
João Seixas <joaoseixas88@gmail.com>
2024-08-25 11:18:14 -0300

    chore: add cors

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -2,9 +2,11 @@ import '../../../setup'
 import express, { json } from "express";
 import { env } from "../../config/env";
 import installRoutes from "./routes";
+import cors from 'cors'
 
 const { PORT } = env;
 const app = express();
 app.use(json());
+app.use(cors());
 installRoutes(app);
 app.listen(PORT, () => console.log(`Server started on: ${PORT}`));
